439933dadaa4244642fdaed9901d2e5f5362315d
RafaGonzatto <rafaelgonzatto181@gmail.com>
2022-09-13 17:45:22 -0300

    primeiraapi

---
+++ App.Persistense/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Persistense
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+        public DbSet<Cidade> Cidades { get; set; }
+    }
+}
